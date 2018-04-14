be71bc8385f4b78fa1b6bef87fdc187b16a0cc0b
Sakutaro Fuchigami <0601aiueo@gmail.com>
2018-04-15 01:51:19 +0900

    curl command preprogram

--- curl-to-python.py
+++ curl-to-python.py
@@ -4,21 +4,36 @@
 # "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true&model=ja-JP_BroadbandModel"
 
 import os.path
-place = input("where is file(Absolute):")#/home/fuchigami/Downloads/voice.flac
-lang = input("select language:")
-if(lang=="japanese"):
-    lang = "ja-JP_BroadbandModel"
-elif(lang=="english"):
-    lang = "en-US_BroadbandModel"
-else:
-    print("write correctry!")
-    exit()
+#import sys
+
+path = input("where is file:")#/home/fuchigami/Downloads/voice.flac
+#name = os.path.dirname(os.path.abspath(__name__))
+#joined_path = os.path.join(name, path)
+#data_path = os.path.normpath(joined_path)
+
+while True:
+    print("select language number ",end="")
+    lang = input("japanese:1 English:2\n")
+    if(lang=="1"):
+        lang = "ja-JP_BroadbandModel"
+        break
+    elif(lang=="2"):
+        lang = "en-US_BroadbandModel"
+        break
+    else:
+        continue
 
 cmd = 'curl -X POST -u 53f2651e-a0f3-4f56-b80b-48be1a01ea3c:fwrQj8UqHIZi\
  --header "Content-Type: audio/flac"\
  --data-binary @{0}\
  "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?continuous=true&model=\
-{1}"'.format(place,lang)
+{1}"'.format(path,lang)
 
 req = os.system(cmd)
+#sys.stdout = open("Speech_to_text.txt","w")
+
 print(req)
+
+#sys.stdout.close()
+#sys.stdout =sys.__stdout__
+#open("Speech_to_text.txt","r").read()
