e8c3690dc7ead48ffbebc67369500675c3c2546b
Sakutaro Fuchigami <0601aiueo@gmail.com>
2018-04-22 02:03:20 +0900

    update

--- api-curl.py
+++ api-curl.py
@@ -1,21 +1,26 @@
 import requests
 import pprint
 
-headers = {
-    'Content-Type': 'audio/flac',
-}
+def request():
+    
+    headers = {'Content-Type': 'audio/flac'}
 
-params = (
-    ('continuous', 'true'),
-    ('model', 'ja-JP_BroadbandModel'),
-)
+    params = {'model':'ja-JP_BroadbandModel'}
 
-auth = ('53f2651e-a0f3-4f56-b80b-48be1a01ea3c', 'fwrQj8UqHIZi')
+    #params = (
+    #    ('continuous', 'true'), URLにあったけど必要ない？
+    #    ('model', 'ja-JP_BroadbandModel'),
+    #)
 
-url = ('https://stream.watsonplatform.net/speech-to-text/api/v1/recognize')
+    auth = ('53f2651e-a0f3-4f56-b80b-48be1a01ea3c', 'fwrQj8UqHIZi')
 
-path = open('/home/fuchigami/Downloads/voice.flac', 'rb').read()
+    url = ('https://stream.watsonplatform.net/speech-to-text/api/v1/recognize')
 
-res = requests.post(url, headers=headers, params=params, data=path,auth=auth)
+    path = open('/home/fuchigami/Downloads/voice.flac', 'rb').read()
 
-pprint.pprint(res.json())
+    res = requests.post(url, headers=headers, params=params, data=path,auth=auth)
+
+    pprint.pprint(res.json())
+
+if __name__=="__main__":
+    request()
